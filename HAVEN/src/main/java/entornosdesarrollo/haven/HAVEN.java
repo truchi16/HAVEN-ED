862123f16b34448667760a88bb732096395a38c7
truchi <truchi@host.docker.internal>
2025-05-15 13:22:15 +0200

    Added a Hello World type message

--- HAVEN/src/main/java/entornosdesarrollo/haven/HAVEN.java
+++ HAVEN/src/main/java/entornosdesarrollo/haven/HAVEN.java
@@ -11,6 +11,6 @@ package entornosdesarrollo.haven;
 public class HAVEN {
 
     public static void main(String[] args) {
-        System.out.println("Hello GIT");
+        System.out.println("Hello World");
     }
 }
